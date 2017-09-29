8c9e3730a43ee1e4a50888fdd3b4e8e917ebe9a7
Josip Ledic <ledicjp@gmail.com>
2017-09-29 18:05:33 +0200

    small fix
    Signed-off-by: Josip Ledic <ledicjp@gmail.com>

--- org.eclipse.winery.topologymodeler.ui/src/app/backend.service.ts
+++ org.eclipse.winery.topologymodeler.ui/src/app/backend.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http, RequestOptions } from '@angular/http';
+import { Headers, Http, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute } from '@angular/router';
 import { isNullOrUndefined } from 'util';
+import { backendBaseURL } from './configuration';
 
 @Injectable()
 export class BackendService {
@@ -18,6 +19,7 @@ export class BackendService {
         isNullOrUndefined(params.uiURL))) {
         this.configuration = params;
         this.getServiceTemplate().subscribe(d => console.log(d));
+        this.getVisuals().subscribe(d => console.log(d));
       }
 
     });
@@ -32,6 +34,19 @@ export class BackendService {
       + this.configuration.id + '/topologytemplate/';
     return this.http.get(url, options)
       .map(res => res.json());
+  }
+
+  getVisuals(): Observable<string> {
+    const headers = new Headers({'Accept': 'application/json'});
+    const options = new RequestOptions({headers: headers});
+    return this.http.get(backendBaseURL + this.activatedRoute.url + '/', options)
+      .map(res => res.json());
+  }
+
+  saveVisuals(data: any): Observable<Response> {
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const options = new RequestOptions({ headers: headers });
+    return this.http.put(backendBaseURL + this.activatedRoute.url + '/', JSON.stringify(data), options);
   }
 }
 
