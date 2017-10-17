9329bc27d786595af304b97fc34f068b4be2e2bc
Oliver Kopp <kopp.dev@gmail.com>
2017-10-17 19:07:22 +0200

    Enable tests running without backend running explicitely

--- org.eclipse.winery.repository.ui.test/src/test/java/org/eclipse/winery/repository/ui/test/TestSettings.java
+++ org.eclipse.winery.repository.ui.test/src/test/java/org/eclipse/winery/repository/ui/test/TestSettings.java
@@ -1,5 +1,8 @@
 package org.eclipse.winery.repository.ui.test;
 
+import org.eclipse.winery.repository.rest.server.WineryUsingHttpServer;
+
+import org.eclipse.jetty.server.Server;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,6 +14,9 @@ public class TestSettings {
 	
 	protected static FirefoxDriver driver;
 
+	private static Server repositoryServer;
+	private static Server repositoryUiServer;
+
 	@BeforeClass
 	public static void init() throws Exception {
 		//System.setProperty("webdriver.gecko.driver", "C:/Users/Franzi/Documents/geckodriver.exe");
@@ -20,11 +26,19 @@ public class TestSettings {
 		
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, SECONDS);
+
+		repositoryServer = WineryUsingHttpServer.createHttpServer();
+		repositoryServer.start();
+
+		repositoryUiServer = WineryUsingHttpServer.createHttpServerForRepositoryUi();
+		repositoryUiServer.start();
 	}
 
 	@AfterClass
 	public static void shutdown() throws Exception {
 		driver.close();
+		repositoryServer.stop();
+		repositoryUiServer.stop();
 	}
 	
 }
