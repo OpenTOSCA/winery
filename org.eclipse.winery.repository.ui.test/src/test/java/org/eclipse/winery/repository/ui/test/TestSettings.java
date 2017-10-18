a0289ad4baccc2b605bf75e6a45ee2d7d59ebff5
Oliver Kopp <kopp.dev@gmail.com>
2017-10-18 02:19:58 +0200

    First try to get UI tests running on Travis

--- org.eclipse.winery.repository.ui.test/src/test/java/org/eclipse/winery/repository/ui/test/TestSettings.java
+++ org.eclipse.winery.repository.ui.test/src/test/java/org/eclipse/winery/repository/ui/test/TestSettings.java
@@ -1,5 +1,7 @@
 package org.eclipse.winery.repository.ui.test;
 
+import java.net.URL;
+
 import org.eclipse.winery.repository.rest.server.WineryUsingHttpServer;
 
 import org.eclipse.jetty.server.Server;
@@ -7,38 +9,59 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class TestSettings {
-	
-	protected static FirefoxDriver driver;
+
+	protected static RemoteWebDriver driver;
 
 	private static Server repositoryServer;
 	private static Server repositoryUiServer;
 
+	private static boolean runsOnSauce;
+
 	@BeforeClass
 	public static void init() throws Exception {
-		//System.setProperty("webdriver.gecko.driver", "C:/Users/Franzi/Documents/geckodriver.exe");
-		System.setProperty("webdriver.gecko.driver", "C:/Users/asst/Desktop/geckodriver.exe");
-		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-		capabilities.setCapability("marionette", true);
-		
-		driver = new FirefoxDriver();
+		String sauceUsername = System.getenv("SAUCE_USERNAME");
+		if (sauceUsername == null) {
+			runsOnSauce = false;
+
+			repositoryServer = WineryUsingHttpServer.createHttpServer();
+			repositoryServer.start();
+
+			repositoryUiServer = WineryUsingHttpServer.createHttpServerForRepositoryUi();
+			repositoryUiServer.start();
+
+			//System.setProperty("webdriver.gecko.driver", "C:/Users/Franzi/Documents/geckodriver.exe");
+			System.setProperty("webdriver.gecko.driver", "C:/Users/asst/Desktop/geckodriver.exe");
+			DesiredCapabilities desiredCapabilities = DesiredCapabilities.firefox();
+			desiredCapabilities.setCapability("marionette", true);
+			driver = new FirefoxDriver(desiredCapabilities);
+		} else {
+			runsOnSauce = true;
+			String sauceAccessKey = System.getenv("SAUCE_ACCESS_KEY");
+			String travisJobNumber = System.getenv("TRAVIS_JOB_NUMBER");
+			String travisBuildNumber = System.getenv("TRAVIS_BUILD_NUMBER");
+
+			DesiredCapabilities desiredCapabilities = DesiredCapabilities.firefox();
+			desiredCapabilities.setCapability("tunnel-identifier", travisJobNumber);
+			desiredCapabilities.setCapability("build", travisBuildNumber);
+			URL hubUrl = new URL("https://" + sauceUsername + ":" + sauceAccessKey + "@localhost:4445/wd/hub");
+
+			driver = new RemoteWebDriver(hubUrl, desiredCapabilities);
+		}
+
 		driver.manage().timeouts().implicitlyWait(10, SECONDS);
-
-		repositoryServer = WineryUsingHttpServer.createHttpServer();
-		repositoryServer.start();
-
-		repositoryUiServer = WineryUsingHttpServer.createHttpServerForRepositoryUi();
-		repositoryUiServer.start();
 	}
 
 	@AfterClass
 	public static void shutdown() throws Exception {
 		driver.close();
-		repositoryServer.stop();
-		repositoryUiServer.stop();
+		if (!runsOnSauce) {
+			repositoryServer.stop();
+			repositoryUiServer.stop();
+		}
 	}
-	
 }
