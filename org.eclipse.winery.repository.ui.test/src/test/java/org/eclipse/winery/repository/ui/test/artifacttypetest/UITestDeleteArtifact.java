f076369496fb205f8e444d94e11a48914ba352b2
Franziska Hutter <franziska_hutter@Ok.de>
2017-10-18 15:05:18 +0200

    Test

--- org.eclipse.winery.repository.ui.test/src/test/java/org/eclipse/winery/repository/ui/test/artifacttypetest/UITestDeleteArtifact.java
+++ org.eclipse.winery.repository.ui.test/src/test/java/org/eclipse/winery/repository/ui/test/artifacttypetest/UITestDeleteArtifact.java
@@ -15,13 +15,11 @@ public class UITestDeleteArtifact extends TestSettings {
 	@Test
 	//Delete artifact on main page
 	public void testDeleteArtifact() throws Exception {
-
 		driver.get("http://localhost:4200/#/other");
 		driver.findElement(By.xpath("//a[@class='btn btn-default'][contains(text(), 'Artifact Types')]")).click();
 		driver.findElement(By.xpath("//div[@class='center']//div[@class='informationContainer']//div[@class='name'][contains(text(), 'Hallo')]")).click();
 		driver.findElement(By.xpath("//button[@class='btn btn-danger'][contains(.,'Delete')]")).click();
 		driver.findElement(By.xpath("//button[@class='btn btn-primary'][contains(.,'Delete')]")).click();
-		//driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
 		Assert.assertTrue("Delete Artifact Type successful", true);
 	}
 }
